8c0e5feade87668bcd7ce41a4fdb5a1f515fd08e
TahinaBemax <153293127+TahinaBemax@users.noreply.github.com>
2024-07-15 18:49:01 +0300

    login terminé

---
+++ application/views/inc/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-md fixed-top navbar-transparency navbar-light" style="background: #ffffff;">
+    <div class="container"><img src="assets/img/ITU_2_-removebg-preview.png" width="158" height="157" style="filter: brightness(81%);">
+        <div><a class="navbar-brand" href="#"><strong>ITU'<span style="color: rgb(0, 191, 98);">CUSTOM</span></strong></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button></div>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">Home</li>
+                <li class="nav-item">Rendez-vous</li>
+                <li class="nav-item">Log Out</li>
+            </ul>
+        </div>
+    </div>
+</nav>
